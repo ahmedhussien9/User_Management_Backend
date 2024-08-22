27aebdc1ebd2744014782d9f9e22b3df37bae770
DESKTOP-EDPGNUP\DELL <ahussien@youxel.com>
2024-08-22 14:38:41 +0300

    [UserManager] Added Auth Module

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,21 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(private authService: AuthService) {
+        // By default, Passport expects fields named 'username' and 'password'.
+        // Here, we're using 'email' instead of 'username'.
+        super({ usernameField: 'email' });
+    }
+
+    async validate(email: string, password: string): Promise<any> {
+        const user = await this.authService.validateUser(email, password);
+        if (!user) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+        return user;
+    }
+}
---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,35 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (!requiredRoles) {
+            return true;
+        }
+
+        const { user } = context.switchToHttp().getRequest();
+
+        console.log(user);
+        // Ensure user and user.roleId are correctly populated
+        if (!user || !user.roleId) {
+            throw new ForbiddenException('User does not have the required roles');
+        }
+
+        // If user.roleId is a single role value
+        return requiredRoles.includes(user.roleId);
+    }
+}
